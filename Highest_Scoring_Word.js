32d7501d617e4e6bd5984aef8174ae0672d3f22c
Vladimir <Leamhein@hotmail.com>
2018-02-15 12:55:59 +0300

    Change sampling method

--- Highest_Scoring_Word.js
+++ Highest_Scoring_Word.js
@@ -3,6 +3,7 @@ function high(x) {
   lettArr = [],
   sum = [],
   pos = 0;
+  console.log(wordArr);
   for (let i = 0; i < wordArr.length; i++) {
     lettArr[i] = wordArr[i].split("");
     sum[i] = 0;
@@ -10,12 +11,17 @@ function high(x) {
       lettArr[i][j] = lettArr[i][j].charCodeAt();
       sum[i] += lettArr[i][j];
     };
+    console.log(sum[i]);
   };
-  for (let i = 0; i < sum.length-1; i++) {
-    if (sum[i] < sum[i+1]) {
-      pos = i+1;
+  var k = 0
+  for (let i = 1; i < sum.length; i++) {
+    if (sum[k] < sum[i]) {
+      k = i;
     };
+    pos = k;
+    console.log(pos);
   };
+  console.log(wordArr[pos]);
   return wordArr[pos];
 };
-console.log(high('mf ra'));
+console.log(high('what time are we climbing up the volcano'));
